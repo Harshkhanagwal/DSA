77890430875794b89393e139af63c29acee53691
Harsh khanagwal <91535787+Harshkhanagwal@users.noreply.github.com>
2022-12-04 04:31:43 -0800

    Add files via upload

---
+++ DSA Basics/Basic_practice_questions/Factorial_of_a_Number_using_Recursion.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+long int factorial(int n);
+int main() {
+    int num;
+    printf("Enter a positive integer: ");
+    scanf("%d",&num);
+
+    printf("Factorial of %d = %ld", num, factorial(num));
+
+    return 0;
+}
+
+long int factorial(int n){
+    if (n>=1){
+        return n*factorial(n-1);
+    }
+    else
+    {
+        return 1;
+    }
+}
